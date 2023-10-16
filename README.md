268751171b49d642d4a2df8d4e7433b0375c149e
Jorge Devia <jorgeadeviam2023@outlook.com>
2023-10-16 15:01:52 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DCNMP
+HOST
+
+127.0.0.1 	    gateway.docker.internal
+127.0.0.1 	    host.docker.internal
+::1         	domain.com
+::1         	sub.domain.com
---
+++ development/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo("Hello from domain.com");
+
+    /*** MySQL  hostname ***/
+    $hostname = 'mariadb';
+    /*** MySQL  username ***/
+    $username = 'root';
+    /*** MySQL  password ***/
+    $password = 'root';
+    /*** MySQL  database ***/
+    $dbname = 'mysql';
+    /*** MySQL  port ***/
+    $port = 3306;
+
+    // Check if the connection to the database was successful.
+    $db = mysqli_connect($hostname, $username, $password, $dbname, $port);
+
+?>
